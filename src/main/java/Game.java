aaded768830143d92c2696de4aa6a7fd5e04a81c
JoaoCostaIFG <joaocosta.tra@outlook.com>
2020-04-13 21:36:04 +0100

    Create listener for terminal window resizes. Try thread stuff.
    The listener stores the current size of the terminal window (and consequently the size of the screen) and whether or not the screen has been resized since the last check. Created a single thread for the main game loop: this is the same as not creating a thread (was just testing/trying the syntax). Next feature should be creating a thread for player input (separate from the main game loop).

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,23 +1,31 @@
 import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.SimpleTerminalResizeListener;
 import com.googlecode.lanterna.terminal.Terminal;
-import org.w3c.dom.Text;
+import com.googlecode.lanterna.terminal.TerminalResizeListener;
+import com.sun.org.apache.xml.internal.utils.res.XResources_zh_CN;
 
 import java.io.IOException;
 
 public class Game {
     private Screen screen;
     private Arena arena;
+    private TerminalResizeHandler resize_handler;
 
     public Game() {
         try {
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            this.screen = new TerminalScreen(terminal);
+            /* 300x100 should be big enough for fullscreen 1080p */
+            TerminalSize init_size = new TerminalSize(300, 100);
+            Terminal terminal = new DefaultTerminalFactory().setInitialTerminalSize(init_size).createTerminal();
+            resize_handler = new TerminalResizeHandler(init_size);
+            terminal.addResizeListener(resize_handler);
 
+            this.screen = new TerminalScreen(terminal);
             screen.setCursorPosition(null); // we don't need a cursor
             screen.startScreen();           // screens must be started
             screen.doResizeIfNecessary();   // resize screen if necessary
@@ -49,19 +57,43 @@ public class Game {
         screen.refresh();
     }
 
+    private void new_frame() {
+        try {
+            this.draw();
+            this.processKey(screen.readInput());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private final int DELAY = 17;
+
     public void run() {
+        long beforeTime, timeDiff, sleep;
+        beforeTime = System.currentTimeMillis();
+
         while (arena.getGameState() != 0) {
             if (arena.getGameState() == 4) {
                 this.arena = new Arena(20, 60);
             }
+            new_frame();
+
+            timeDiff = System.currentTimeMillis() - beforeTime;
+            sleep = DELAY - timeDiff;
+            if (sleep < 0)
+                sleep = 2;
+
             try {
-                this.draw();
-                this.processKey(screen.readInput());
-            } catch (IOException e) {
-                e.printStackTrace();
+                Thread.sleep(sleep);
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted: %s" + e.getMessage());
             }
+
+            beforeTime = System.currentTimeMillis();
         }
 
+        // close game
         try {
             screen.close();
         } catch (IOException e) {
