da816296754705e8d19252b09a2519cb8e8f6d34
Jtp <joaolucassilvamartins@gmail.com>
2020-04-17 00:45:03 +0100

    Added Tests (broken :)).

--- src/test/java/RoomTests.java
+++ src/test/java/RoomTests.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+import room.Room;
+import room.element.Skane;
+import room.element.Wall;
+
+import static org.junit.Assert.assertEquals;
 
 public class RoomTests {
+    private Room room = new Room(60, 30);
+
+    @Test
+    public void creation() {
+        assertEquals(1, 1);
+        assertEquals(room.getWidth(), 60);
+        assertEquals(room.getHeight(), 30);
+
+        Wall wall = new Wall(2, 3);
+        Skane skane = new Skane(1, 5, 10, 5, 5, 3);
+
+        room.addElement(wall);
+        room.addElement(skane);
+
+        assertEquals(room.getWalls().size(), 1);
+        assertEquals(room.getSkane(), skane);
+    }
 }
---
+++ src/test/java/SkaneTests.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SkaneTests {
+    @Test
+    public void move() {
+        assertEquals(1, 1);
+    }
+}
