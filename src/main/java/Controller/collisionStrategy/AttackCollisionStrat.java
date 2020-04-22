634f5e13e6ee4a5c8fb09c963a5d6b9fb4fb4a64
Jtp <joaolucassilvamartins@gmail.com>
2020-04-22 13:26:19 +0100

    Working on collision strategy and controller atm. Incomplete

--- src/main/java/Controller/collisionStrategy/AttackCollisionStrat.java
+++ src/main/java/Controller/collisionStrategy/AttackCollisionStrat.java
@@ -1,4 +1,24 @@
 package Controller.collisionStrategy;
 
-public class AttackCollisionStrat {
+import room.Position;
+import room.element.*;
+
+public class AttackCollisionStrat extends CollisionStrategy<EntityQueMorde, Entity> {
+    public AttackCollisionStrat(EntityQueMorde colliding_elem) {
+        super(colliding_elem);
+    }
+
+    @Override
+    public EntityQueMorde getColliding_elem() {
+        return super.getColliding_elem();
+    }
+
+    // TODO cleanup dead enemies later
+    @Override
+    public void handle(Entity element, Position wanted_pos) {
+        element.takeDamage(colliding_elem.getAtk());
+        if (!element.isAlive())
+            colliding_elem.setPos(wanted_pos);
+    }
+
 }
--- src/main/java/Controller/collisionStrategy/CollisionStrategy.java
+++ src/main/java/Controller/collisionStrategy/CollisionStrategy.java
@@ -1,4 +1,19 @@
 package Controller.collisionStrategy;
 
-public abstract class collisionStrategy {
+import room.Position;
+import room.element.CollidableElement;
+import room.element.MovableElement;
+
+public abstract class CollisionStrategy<T extends MovableElement, S extends CollidableElement> {
+    protected T colliding_elem;
+
+    public CollisionStrategy(T colliding_elem) {
+        this.colliding_elem = colliding_elem;
+    }
+
+    public T getColliding_elem() {
+        return colliding_elem;
+    }
+
+    public abstract void handle(S element, Position wanted_pos);
 }
