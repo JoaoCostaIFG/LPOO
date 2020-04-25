add3b54fd5b17c3861c8106a7d56414a187137a4
JoaoCostaIFG <joaocosta.work@posteo.net>
2020-04-26 00:52:11 +0100

    Trying fog of war.

--- src/main/java/gui/Drawer.java
+++ src/main/java/gui/Drawer.java
@@ -1,12 +1,10 @@
 package gui;
 
+import room.Position;
 import room.Room;
-import room.element.Civilian;
-import room.element.Entity;
-import room.element.MeleeGuy;
+import room.element.*;
 import room.element.skane.Skane;
 import room.element.skane.SkaneBody;
-import room.element.Wall;
 import com.googlecode.lanterna.*;
 import com.googlecode.lanterna.graphics.TextGraphics;
 
@@ -23,15 +21,16 @@ public class Drawer implements GraphicsDrawer {
     private static final TextColor purple = fromString("#8558AD");
     private static final TextColor red = fromString("#844F4E");
 
-    private static final TextCharacter bgChar = new TextCharacter(' ', bg, bg);
-    private static final TextCharacter civieChar = new TextCharacter('C', blue, bg);
-    private static final TextCharacter meleeChar = new TextCharacter('M', red, bg);
-    private static final TextCharacter wallChar = new TextCharacter('#', purple, bg);
-
+    private static final char bgChar = ' ';
     private static final char skaChar = 'S';
     private static final char skaBuryChar = 'X';
     private static final char skaBodyChar = 'o';
     private static final char skaBodyBuryChar = 'x';
+    private static final char civieChar = 'C';
+    private static final char meleeChar = 'M';
+    private static final char wallChar = '#';
+
+    private static final int skaFov = 5;
 
     private TextGraphics gra;
 
@@ -48,7 +47,6 @@ public class Drawer implements GraphicsDrawer {
     @Override
     public void drawSkane(Skane ska) {
         gra.enableModifiers(SGR.BOLD);
-        gra.setBackgroundColor(bg);
 
         double oxyPerc = (double) (ska.getMaxOxygenLevel() - ska.getOxygenLevel()) / ska.getMaxOxygenLevel();
         long numSpotsToFill = Math.round((ska.getSize() + 1) * oxyPerc);
@@ -71,33 +69,64 @@ public class Drawer implements GraphicsDrawer {
 
     @Override
     public void drawWall(Wall wall) {
+        gra.setForegroundColor(purple);
         gra.setCharacter(wall.getX(), wall.getY(), wallChar);
     }
 
     @Override
     public void drawCivie(Civilian civie) {
+        gra.setForegroundColor(blue);
         gra.setCharacter(civie.getX(), civie.getY(), civieChar);
     }
 
     @Override
     public void drawMelee(MeleeGuy melee) {
+        gra.setForegroundColor(red);
         gra.setCharacter(melee.getX(), melee.getY(), meleeChar);
+    }
+
+    private void fillBg(Room room, Position skaPos, boolean isSkaBury) {
+        gra.setBackgroundColor(isSkaBury ? bgDark : bg);
+        gra.setForegroundColor(isSkaBury ? bgDark : bg);
+
+        gra.fillRectangle(new TerminalPosition(0, 0),
+                new TerminalSize(room.getWidth(), room.getHeight()), bgChar);
+
+        if (isSkaBury) { // if skane is buried, draw FOV around head
+            gra.setBackgroundColor(bg);
+            gra.setForegroundColor(bg);
+            for (int i = skaPos.getX() - skaFov; i < skaPos.getX() + skaFov; ++i) {
+                for (int j = skaPos.getY() - skaFov; j < skaPos.getY() + skaFov; ++j) {
+                    if (Math.pow(skaPos.getX() - i, 2) + Math.pow(skaPos.getY() - j, 2) < skaFov * skaFov)
+                        gra.setCharacter(i, j, bgChar);
+                }
+            }
+            gra.setBackgroundColor(bgDark);
+            gra.setForegroundColor(bgDark);
+        }
+    }
+
+    private boolean isOutsideSkaFov(Element e, Position skaPos, boolean isSkaBury) {
+        return (isSkaBury && e.getPos().dist(skaPos) > skaFov);
     }
 
     @Override
     public void drawRoom(Room room) {
-        gra.fillRectangle(new TerminalPosition(0, 0),
-                new TerminalSize(room.getWidth(), room.getHeight()), bgChar);
+        /* for (Scent s : room.getSkane().getScentTrail()) gra.setCharacter(s.getX(), s.getY(), civieChar); */
 
-        /*
-        for (Scent s : room.getSkane().getScentTrail())
-            gra.setCharacter(s.getX(), s.getY(), civieChar);
-         */
+        boolean isSkaBury = room.isSkaneBury();
+        Position skaPos = room.getSkanePos();
+        fillBg(room, skaPos, isSkaBury);
 
-        for (Wall wall : room.getWalls())
-            drawWall(wall);
+        for (Wall wall : room.getWalls()) {
+            if (!isOutsideSkaFov(wall, skaPos, isSkaBury))
+                drawWall(wall);
+        }
 
         for (Entity e : room.getEnemies()) {
+            if (isOutsideSkaFov(e, skaPos, isSkaBury))
+                continue;
+
             if (e instanceof Civilian) drawCivie((Civilian) e);
             else if (e instanceof MeleeGuy) drawMelee((MeleeGuy) e);
         }
