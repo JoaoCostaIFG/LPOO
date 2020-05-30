9eed6efa98cfa8b140261e9de66053bd218d43b1
JoaoCostaIFG <joaocosta.work@posteo.net>
2020-05-30 15:33:47 +0100

    k

--- src/main/java/org/g73/skanedweller/model/colliders/Collider.java
+++ src/main/java/org/g73/skanedweller/model/colliders/Collider.java
@@ -1,18 +1,33 @@
 package org.g73.skanedweller.model.colliders;
 
 import org.g73.skanedweller.model.Position;
-import org.g73.skanedweller.model.element.Element;
 import org.g73.skanedweller.observe.Observer;
 
-public abstract class Collider extends Element implements Observer<Position> {
+public abstract class Collider implements Observer<Position> {
     private Position position;
 
     public Collider(Position pos) {
-        super(pos);
+        this.position = pos;
     }
 
     public Collider(int x, int y) {
-        super(x, y);
+        this(new Position(x, y));
+    }
+
+    public void setPos(Position newPos) {
+        this.position = newPos;
+    }
+
+    public Position getPos() {
+        return this.position;
+    }
+
+    public int getX() {
+        return this.position.getX();
+    }
+
+    public int getY() {
+        return this.position.getY();
     }
 
     public abstract boolean collidesWith(Collider col);
